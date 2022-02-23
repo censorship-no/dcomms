8519c8eca834dff0c37fe278d929b2609a6be022
Keith Petkus <keith@keithp.net>
2022-02-23 11:34:58 -0500

    Add recent changes

--- provision.sh
+++ provision.sh
@@ -7,20 +7,27 @@ then
   exit 1
 fi
 
+# Replace dots with dashes
+export DWEB_FRIENDLY_DOMAIN="${DWEB_DOMAIN//./_}"
 echo "## Removing existing dwebstack"
-docker stack rm dwebstack
+docker stack rm dwebstack-${DWEB_FRIENDLY_DOMAIN}
+docker network rm caddy-${DWEB_FRIENDLY_DOMAIN}
 echo "## Sleeping for 10 seconds"
 sleep 10
 echo "## Removing synapse-data"
 echo "## Generating synapse config"
 docker run -it --rm \
-    --mount type=volume,src=synapse_data,dst=/data \
+    --mount type=volume,src=synapse_data_tmp,dst=/data \
     -e SYNAPSE_SERVER_NAME=$DWEB_DOMAIN \
     -e SYNAPSE_REPORT_STATS=no \
-    matrixdotorg/synapse:latest generate
+    -e SYNAPSE_DATA_DIR=/data \
+    matrixdotorg/synapse:v1.53.0 generate
 echo "## Copying config files into docker swarm configs"
-cp /var/lib/docker/volumes/synapse-data/_data/homeserver.yaml conf/synapse/$DWEB_DOMAIN.homeserver.yaml
-cp /var/lib/docker/volumes/synapse-data/_data/$DWEB_DOMAIN.signing.key conf/synapse/$DWEB_DOMAIN.signing.key
-cp /var/lib/docker/volumes/synapse-data/_data/$DWEB_DOMAIN.log.config conf/synapse/$DWEB_DOMAIN.log.config
+cp -a /var/lib/docker/volumes/synapse_data_tmp/_data/homeserver.yaml ./conf/synapse/$DWEB_DOMAIN.homeserver.yaml
+cp -a /var/lib/docker/volumes/synapse_data_tmp/_data/$DWEB_DOMAIN.signing.key ./conf/synapse/$DWEB_DOMAIN.signing.key
+cp -a /var/lib/docker/volumes/synapse_data_tmp/_data/$DWEB_DOMAIN.log.config ./conf/synapse/$DWEB_DOMAIN.log.config
+
+#sed -i 's/data\/media_store/data2\/media_store/g' ./conf/synapse/$DWEB_DOMAIN.homeserver.yaml
+
 echo "## Provisioning dwebstack"
-DWEB_DOMAIN=$DWEB_DOMAIN docker stack deploy -c docker-compose.yml dwebstack
+DWEB_DOMAIN=$DWEB_DOMAIN DWEB_FRIENDLY_DOMAIN=$DWEB_FRIENDLY_DOMAIN docker stack deploy -c docker-compose.yml dwebstack-${DWEB_FRIENDLY_DOMAIN}
