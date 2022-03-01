2a4fc47373a3b01b26de43cdd573c7bfadbf545c
Your Name <you@example.com>
2022-03-01 17:22:08 +0300

    ADD nuxt

---
+++ site/front/pages/index.vue
@@ -0,0 +1,246 @@
+<template>
+  <header class="header container">
+    <div class="row">
+      <img class="logo" src="./bridge-icon.png" alt="" />
+      <h1>dComms</h1>
+    </div>
+    <div class="lang">
+      <a href="./index_ru.html">RU</a> / <a href="./index_en.html">EN</a>
+    </div>
+  </header>
+  <main class="container">
+    <h2 class="title">
+      Децентралізовані комунікації, які працюють з інтернетом або без нього
+    </h2>
+    <div>Інструменти комунікації, що працюють в локальної мережі. Необхідні, аби залишатися на зв’язку з друзями та
+      родиною, а також коли Telegram, Whatsapp, FB будуть недоступні у вашому регіоні.
+      Наведені нижче сервери розташовані в Україні на основі інфраструктури (мереж) місцевих інтернет-провайдерів. Якщо
+      у вас є доступ до цієї локальної мережі, ви можете спілкуватися між собою!
+    </div>
+    <div>
+      <a href="https://kyiv.dcomm.net.ua/">Київ</a>
+      <a href="https://odessa.dcomm.net.ua">Одеса</a>
+      <a href="https://kharkiv.dcomm.net.ua">Харків</a>
+      <div>У разі відключення Інтернету ми дамо можливість завантажити усе програмне забезпечення (cофт) та документацію
+        з локальних серверів.
+      </div>
+    </div>
+    <div>
+      <img height="16" style="margin-right: 5px" class="logo" src="./logo/Matrix_logo.png" alt="Matrix" />
+      <div>Обмін повідомленнями, що підтримують наскрізне шифрування, приватні чати, приватні кімнати та публічні
+        кімнати. Зважайте, що реєстрація доступна для всіх – будьте уважні щодо ідентифікації особи користувача, перш
+        ніж довіряти цьому джерелу.
+
+        Приватні групи доступні лише їхнім засновникам і запрошеним користувачам. Діє принцип зв'язаного сервісу – люди
+        на одному сервері можуть спілкуватися з людьми, підключеними до іншого.
+
+
+        Для публічних груп діє модерація </div>
+      <div>Як отримати доступ з комп’ютера (десктоп):
+      </div>
+      <div>
+        <div> Обмін повідомленнями:
+        </div>
+        <div>
+          <div> <a href="">Київ</a>        <div>інструкція PDF</div>
+ </div>  <div> <a href="">Харків</a>        <div>інструкція PDF</div>
+ </div>  <div> <a href="">Одеса</a>        <div>інструкція PDF</div>
+ </div>
+        </div>
+
+
+      </div>
+      <div>Як отримати доступ зі смартфона:
+      </div>
+      <div>
+        Додаток для смартфона: Element, (локальне скачування)
+        <div>
+          <div>Київ сервер: matrix.kyiv.dcomm.net.ua</div>
+          <div><a href="">інструкція PDF</a></div>
+          <div>Харків сервер: matrix.kharkiv.dcomm.net.ua</div>
+          <div><a href="">інструкція PDF</a></div>
+          <div>Одеса сервер: matrix.odessa.dcomm.net.ua</div>
+          <div><a href="">інструкція PDF</a></div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="table">
+
+
+      <div class="row">
+        <div class="rubric-title">Переписка</div>
+        <div>
+          <span class="mobile-rubric">Утіліта:</span>
+          <a target="_blank" href="https://matrix.org">Matrix</a>
+        </div>
+        <div><span class="mobile-rubric">Клієнт:</span> Веб-версія</div>
+        <div>
+          <span class="mobile-rubric">Доступ:</span>
+          <a target="_blank" href="https://chat.kyiv.dcomm.net.ua/">Київ;</a>
+          <a target="_blank" href="https://chat.odessa.dcomm.net.ua">Одеса;</a>
+          <a target="_blank" href="https://chat.kharkiv.dcomm.net.ua">Харків</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Документація:</span>
+          <a target="_blank" href="./docs/matrix/kyiv.html">Київ</a>&nbsp;<a target="_blank"
+            href="docs/matrix/kyiv.pdf">[PDF]</a>;
+          <a target="_blank" href="./docs/matrix/odessa.html">Одеса</a>&nbsp;<a target="_blank"
+            href="docs/matrix/odessa.pdf">[PDF]</a>;
+          <a target="_blank" href="./docs/matrix/kharkiv.html">Харків</a>&nbsp;<a target="_blank"
+            href="docs/matrix/kharkiv.pdf">[PDF]</a>
+        </div>
+      </div>
+      <div class="row">
+        <div></div>
+
+        <div>
+          <span class="mobile-rubric">Утіліта:</span>
+          <img height="16" style="margin-right: 5px" class="logo" src="./logo/logo-mark-primary.svg" alt="Element" />
+          <a target="_blank" href="https://element.io">Element</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Клієнт:</span>
+          <a target="_blank" href="https://element.io/get-started#download">Android; iOS; PC
+          </a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Доступ:</span>
+          <i>домашнього сервера:</i><br>
+          Київ: https://matrix.kyiv.dcomm.net.ua<br>
+          Одеса: https://matrix.odessa.dcomm.net.ua<br>
+          Харків: https://matrix.kharkiv.dcomm.net.ua<br><br>
+          <a href="/package/element/files/" target="_blank">Локальне скачування</a>
+        </div>
+        <div>
+          <a target="_blank" href="./docs/element/kyiv.html">Київ</a>&nbsp;<a target="_blank"
+            href="./docs/element/kyiv.pdf">[PDF]</a>;
+          <a target="_blank" href="./docs/element/odessa.html">Одеса</a>&nbsp;<a target="_blank"
+            href="./docs/element/odessa.pdf">[PDF]</a>;
+          <a target="_blank" href="./docs/element/kharkiv.html">Харків</a>&nbsp;<a target="_blank"
+            href="./docs/element/kharkiv.pdf">[PDF]</a>
+        </div>
+      </div>
+
+      <div class="row">
+        <div></div>
+        <div>
+          <span class="mobile-rubric">Утіліта:</span>
+
+          <img height="16" style="margin-right: 5px" class="logo" src="./logo/deltachat.svg" alt="Delta Chat" />
+          <a href="https://delta.chat">Delta Chat</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Клієнт:</span>
+
+          <a target="_blank" href="https://get.delta.chat/">Android, iOS, Desktop</a>
+        </div>
+
+        <div>
+          <span class="mobile-rubric">Доступ:</span>
+
+          <a href="docs/deltachat/kyiv-invite.png">Київ;</a>
+          <a href="docs/deltachat/odessa-invite.png">Одеса;</a>
+          <a href="docs/deltachat/kharkiv-invite.png">Харків</a><br>
+          <a href="/package/deltachat/files/" target="_blank">Локальне скачування</a>
+        </div>
+        <div><span class="mobile-rubric">Документація:</span>
+          <a target="_blank" href="docs/deltachat/deltachat-manual.pdf">[PDF]</a>
+        </div>
+      </div>
+
+      <div class="row">
+        <div></div>
+
+        <div>
+          <span class="mobile-rubric">Утіліта:</span>
+          <img height="16" style="margin-right: 5px" class="logo" src="./logo/briar.png" alt="Briar Project" />
+          <a href="https://briarproject.org">Briar</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Клієнт:</span>
+          <a target="_blank" href="https://briarproject.org/download-briar/">Android</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Доступ:</span>
+
+          <a href="/package/briar/files/" target="_blank">Локальне скачування</a>
+        </div>
+        <div><span class="mobile-rubric">Документація:</span>
+          <a target"_blank" href="docs/briar/index.html">Guide</a>&nbsp;<a target="_blank"
+            href="docs/briar/briar.pdf">[PDF]</a></div>
+      </div>
+      <div class="row">
+        <div class="rubric-title">Перегляд сайтів</div>
+
+        <div>
+          <span class="mobile-rubric">Утіліта:</span>
+          <img height="16" style="margin-right: 5px" class="logo" src="./logo/ceno.png" alt="CENO Browser" />
+          <a href="https://censorship.no">CENO Browser</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Клієнт:</span>
+          <a target="_blank" href="https://censorship.no/download">Android
+          </a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Доступ:</span>
+
+          <a href="/package/ceno/files/" target="_blank">Локальне скачування</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Документація:</span><a href="docs/ceno/index.html">Посібник користувача</a>
+        </div>
+      </div>
+      <div class="row">
+        <div></div>
+
+        <div>
+          <span class="mobile-rubric">Утіліта:</span>
+          <img height="16" style="margin-right: 5px" class="logo" src="./logo/512x512bb.jpg" alt="Newnode" />
+          <a href="https://newnode.com">Newnode</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Клієнт:</span>
+          <a target="_blank" href="https://newnode.com/download-vpn">Android; iOS</a>
+        </div>
+        <div>
+          <span class="mobile-rubric">Доступ:</span>
+
+          <a href="/package/newnode/" target="_blank">Локальне скачування</a>
+        </div>
+        <div><span class="mobile-rubric">Документація:</span>
+          <a target="_blank" href="docs/newnode/index.html">швидкий огляд</a></div>
+      </div>
+    </div>
+  </main>
+  <footer class="container footer">
+    <img width="100" class="logo" src="./logo_ceno.png" alt="CENO-project" />
+    Ініциатива —
+    <a class="link" target="_blank" href="https://censorship.no" style="margin-left: 5px">
+      censorship.no</a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
